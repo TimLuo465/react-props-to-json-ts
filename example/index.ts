049255b7440a8748bb1708319ef3be460b817ead
timluo465 <timluo465@gmail.com>
2021-03-08 16:42:22 +0800

    chore: Optimize examples

--- example/index.ts
+++ example/index.ts
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const propToSchema = require('../dist/react-props-to-json-ts.cjs.js');
+const propsToJSON = require('../dist/react-props-to-json-ts.cjs.js');
+const result = propsToJSON(`${__dirname}/index.tsx`);
 
-// 参数为目标文件地址
-const apiInfo = propToSchema(`${__dirname}/index.tsx`);
-fs.writeFileSync(path.join(__dirname, 'schema.json'), JSON.stringify(apiInfo, null, 2));
+fs.writeFileSync(path.join(__dirname, 'schema.json'), JSON.stringify(result, null, 2));
